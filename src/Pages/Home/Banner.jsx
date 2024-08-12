f9b618a678e6ab169c89e36738c5ee8903278f75
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-12 22:43:19 +0600

    add navbar and section and banner

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,3 +1,4 @@
+import { curve } from "../../assets";
 import Section from "../../Component/Section";
 
 
@@ -8,8 +9,9 @@ const Banner = () => {
    customPaddings id='hero'>
 <div className="container relative">
     <div className="relative z-1 max--w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
-        <h1>
-            Explore
+        <h1 className="h1 mb-6">
+            Explore the Possibilities of AI Chatting with BrainFlowCurve{""}
+            <img src={curve} alt="" />
         </h1>
     </div>
 
