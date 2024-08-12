cf028c754a1284f9fc187ef9e6fe15af35d7f6a8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-12 22:36:25 +0600

    index.js file

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -7,7 +7,11 @@ const Banner = () => {
    <Section className="pt-[12rem] -mt-[5.25]" crosses crossesOffset="lg:translate-y-[5.25]"
    customPaddings id='hero'>
 <div className="container relative">
-    <div className="relative z-1 max-"></div>
+    <div className="relative z-1 max--w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+        <h1>
+            Explore
+        </h1>
+    </div>
 
 </div>
    </Section>
