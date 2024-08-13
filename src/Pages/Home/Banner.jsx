00a7d370c55e14d1b92bec9617e6ec91718e8f3b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-13 10:56:37 +0600

    add navbar and section and banner

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,23 +1,34 @@
 import { curve } from "../../assets";
 import Section from "../../Component/Section";
-
+import Button from "../../Component/Button"
 
 const Banner = () => {
     return (
-   <>
-   <Section className="pt-[12rem] -mt-[5.25]" crosses crossesOffset="lg:translate-y-[5.25]"
-   customPaddings id='hero'>
-<div className="container relative">
-    <div className="relative z-1 max--w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
-        <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with BrainFlowCurve{""}
-            <img src={curve} alt="" />
-        </h1>
-    </div>
+        <>
+            <Section className="pt-[12rem] -mt-[5.25]" crosses crossesOffset="lg:translate-y-[5.25]"
+                customPaddings id='hero'>
+                <div className="container relative">
+                    <div className="relative z-1 max--w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+                        <h1 className="h1 mb-6">
+                            Explore the Possibilities of AI Chatting with BrainFlowCurve
+                            <span className="inline-block relative">BrainFlow{""}
+                                <img src={curve} className="absolute top-full left-0 w-full xl:-mt-1"
+                                width={624}
+                                height={28}
+                                alt="curve"/>
+                            </span>
+                        </h1>
+                        <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">Unleash the power of Ai within BrainFlow.
+                            Upgrade your Productivity with brainFlow,the open AI chat app
+                        </p>
+                        <Button href="/pricing" white>
+                            Get Started
+                        </Button>
+                    </div>
 
-</div>
-   </Section>
-   </>
+                </div>
+            </Section>
+        </>
     );
 };
 
