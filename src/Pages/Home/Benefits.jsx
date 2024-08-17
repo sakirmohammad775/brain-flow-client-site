034c8e1ee4e6a34d6dc44b62c818b018171b134e
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-17 10:59:08 +0600

    add benefits section

--- src/Pages/Home/Benefits.jsx
+++ src/Pages/Home/Benefits.jsx
@@ -3,7 +3,7 @@ import { benefits } from "../../Constant";
 import Heading from "./Heading";
 import Arrow from '../../assets/svg/Arrow'
 import { GradientLight } from "../../Component/design/Benefits";
-
+import ClipPath from "../../assets/svg/ClipPath"
 const Benefits = () => {
     return (
         <>
@@ -30,6 +30,17 @@ const Benefits = () => {
                                             </div>
                                         </div>
                                         {item.light && <GradientLight/>}
+                                        <div className="absolute inset-0.5 bg-n-8"style={{clipPath:"url(#benefits)"}}>
+                                            <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                                                {item.imageUrl &&(<img
+                                                src={item.imageUrl}
+                                                width={380}
+                                                height={362}
+                                                alt={item.title}
+                                                className="w-full h-full object-cover"></img>)}
+                                            </div>
+                                        </div>
+                                        <ClipPath/>
                                     </div>
                                 </>
                             ))
--- src/Pages/Home/Heading.jsx
+++ src/Pages/Home/Heading.jsx
@@ -2,7 +2,7 @@
 const Heading = ({className,title}) => {
     return (
         <div className={`${className} max-w-[50rem] mx-auto `}>
-            {title && <h2 className="h2">{title}</h2>}
+            {title && <h2 className="h2 mb-10">{title}</h2>}
         </div>
     );
 };
