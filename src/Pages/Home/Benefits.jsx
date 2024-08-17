432142f5cf0ed3a39b33745df43bfe9d73d9fc86
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-17 10:28:16 +0600

    add benefits section

--- src/Pages/Home/Benefits.jsx
+++ src/Pages/Home/Benefits.jsx
@@ -1,30 +1,42 @@
 import Section from "../../Component/Section";
 import { benefits } from "../../Constant";
 import Heading from "./Heading";
-
+import Arrow from '../../assets/svg/Arrow'
+import { GradientLight } from "../../Component/design/Benefits";
 
 const Benefits = () => {
     return (
         <>
-        <Section id="features">
-            <div className="container relative z--2">
-                <Heading className="md:mx-w-md lg:max-w-2xl" title="Chat Smarter, Not Harder with BrainFlow"></Heading>
-                <div 
-                className="flex flex-wrap gap-10 mb-10">
-{
-    benefits.map((item)=>(
-        <>
-        <div key={item.id}>
-            <div>
-                <h5>{item.title}</h5>
-            </div>
-        </div>
-        </>
-    ))
-}
+            <Section id="features">
+                <div className="container relative z--2">
+                    <Heading className="md:mx-w-md lg:max-w-2xl" title="Chat Smarter, Not Harder with BrainFlow"></Heading>
+                    <div
+                        className="flex flex-wrap gap-10 mb-10">
+                        {
+                            benefits.map((item) => (
+                                <>
+                                    <div className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] "
+                                    style={{backgroundImage:`url(${item.backgroundUrl})`}}
+                                    key={item.id}>
+                                        <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                                            <h5 className="h2 mb-5">{item.title}</h5>
+                                            <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                                            <div className="flex items-center mt-auto">
+                                                <img src={item.iconUrl}
+                                                width={48}
+                                                height={48} alt={item.title} />
+                                                <p className="ml-auto font-bold text-n-1 uppercase -tracking-wider">Explore more</p>
+                                                <Arrow/>
+                                            </div>
+                                        </div>
+                                        {item.light && <GradientLight/>}
+                                    </div>
+                                </>
+                            ))
+                        }
+                    </div>
                 </div>
-            </div>
-        </Section>
+            </Section>
         </>
     );
 };
