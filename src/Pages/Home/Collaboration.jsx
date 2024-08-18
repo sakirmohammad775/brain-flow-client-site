2e1d87065110f4c094f073afd8b1e3c55f530e8d
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-18 14:57:43 +0600

    add benefits section

--- src/Pages/Home/Collaboration.jsx
+++ src/Pages/Home/Collaboration.jsx
@@ -26,7 +26,9 @@ const Collaboration = () => {
                         <div className="lg:ml-auto xl:w-[38rem] mt-4">
                             <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">{collabText}</p>
                             <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
-                               
+                                <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+                                    <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient "></div>
+                                </div>
                             </div>
                         </div>
                     </div>
