da19993713a3cf6ab9d44467c4054dd61c7c0bdc
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-18 18:00:47 +0600

    add card

--- src/Pages/Home/Collaboration.jsx
+++ src/Pages/Home/Collaboration.jsx
@@ -1,4 +1,4 @@
-import { check } from "../../assets";
+import {   brainwaveSymbol, check } from "../../assets";
 import Section from "../../Component/Section"
 import { collabContent, collabText } from "../../Constant";
 import Button from "../../Component/Button"
@@ -26,8 +26,13 @@ const Collaboration = () => {
                         <div className="lg:ml-auto xl:w-[38rem] mt-4">
                             <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">{collabText}</p>
                             <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
-                                <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
-                                    <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient "></div>
+                                <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full ">
+                                    <div className="w-[6rem] aspect-square m-auto p-[0.2rem]  rounded-full bg-conic-gradient">
+                                        <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                                            <img src={brainwaveSymbol} width={48}
+                                            height={48} alt="brainwave" />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
